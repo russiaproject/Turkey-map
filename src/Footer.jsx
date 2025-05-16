609b17a5a7a698cbf82f7b361e744614f050e516
russiaproject <ru5siaproject@yandex.com>
2025-05-17 00:00:38 +0300

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import matrushka from "./images/matrushka.png";
+import anitkabir from "./images/anitkabir.png";
+import red from "./images/red.png";
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="footer-custom position-relative py-5 mt-5">
+      <div class="position-absolute bottom-0 start-25 ms-5">
+        <img src={red} alt="" className='redBina'/>
+        </div>
+        <div class="position-absolute top-0 end-0 me-5 rotateMat">
+        <img src={matrushka} alt="" className='matrushka'/>
+        </div>
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-12 col-md-4">
+            <h5 className="footer-title mb-3">Русский Дом Анкара</h5>
+            <p className="footer-text">Hilal mah., Rabindranath Tagore cad., n.68 Çankaya, Ankara</p>
+            <p className="footer-text">
+              <a href="mailto:turkey@rs.gov.ru" className="footer-link">
+                turkey@rs.gov.ru
+              </a>
+            </p>
+            <p className="footer-text">
+              <a href="tel:+903124420218" className="footer-link">
+                +90 312 442 02 18
+              </a>
+            </p>
+          </div>
+          <div className="col-12 col-md-4 text-center text-md-start">
+            <h5 className="footer-title mb-3">Hızlı Gezinme</h5>
+            <nav className="d-flex flex-column align-items-center align-items-md-start">
+              <a href="#" className="footer-link mb-1">Ana Sayfa</a>
+              <a href="#" className="footer-link mb-1">Rusevi Türkiye Hakkında</a>
+              <a href="#" className="footer-link mb-1">Harita</a>
+              <a href="#" className="footer-link mb-1">İletişim</a>
+
+            </nav>
+          </div>
+          <div className="col-12 col-md-4 text-center text-md-start">
+            <h5 className="footer-title mb-3">Rusevi Sosyalde!</h5>
+            <div className="d-flex flex-column align-items-center align-items-md-start">
+               <a href="#" className="footer-link mb-1">Twitter</a>
+               <a href="#" className="footer-link mb-1">Instagram</a>
+               <a href="#" className="footer-link mb-1">Telegram</a>
+               <a href="#" className="footer-link mb-1">VK</a>
+
+
+            </div>
+          </div>
+        </div>
+        <div className="footer-copyright text-center mt-5 pt-4">
+          © {new Date().getFullYear()} Русский Дом Анкара. Tüm hakları saklıdır.
+          <br />
+          <Link to="/credit" className='katki'>Bu Siteye Katkısı Olanları Görün!</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
