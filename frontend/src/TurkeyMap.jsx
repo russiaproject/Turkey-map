39ca9ab7ed52a61aff12fef293af12e1c7352d07
russiaproject <ru5siaproject@yandex.com>
2025-05-30 16:22:37 +0300

    Update TurkeyMap.jsx

--- frontend/src/TurkeyMap.jsx
+++ frontend/src/TurkeyMap.jsx
@@ -1052,9 +1052,9 @@ const TurkeyMap = () => {
                         
                         if (cityCode === "35") {
                           circleOffsetX = 3;
-                          circleOffsetY = 10; 
+                          circleOffsetY = 18; 
                           textOffsetX = 3; 
-                          textOffsetY = 14;
+                          textOffsetY = 22;
                         }
                         
                         else if (cityCode === "06") {
