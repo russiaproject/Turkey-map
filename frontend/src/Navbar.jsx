26dedcf86a1a19cde1b37cf1109e2bbe19d1f421
russiaproject <ru5siaproject@yandex.com>
2025-06-11 23:20:03 +0300

    Update Navbar.jsx

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -148,7 +148,7 @@ const Navbar = ({ rusevi, turkiyeRusyaBayrak }) => {
               <Link to="/turkiye-rusya-dostlugu" className="nav-link-item text-white text-decoration-none py-2 d-block" onClick={toggleSidebar}><i className="fa-solid fa-handshake"></i> Türkiye ve Rusya Dostluğu </Link>
             </li>
             <li className="nav-item">
-              <Link to="/turkiyede-rus-izleri" className="nav-link-item text-white text-decoration-none py-2 d-block" onClick={toggleSidebar}><i className="fa-solid fa-heart"></i> Türkiye'de ki Rus İzleri </Link>
+              <Link to="/turkiyede-rus-izleri" className="nav-link-item text-white text-decoration-none py-2 d-block" onClick={toggleSidebar}><i className="fa-solid fa-heart"></i> Türkiye'deki Rus İzleri </Link>
             </li>
              <li className="nav-item">
               <Link to="/vize" className="nav-link-item text-white text-decoration-none py-2 d-block" onClick={toggleSidebar}><i className="fa-solid fa-building-columns"></i> Vize </Link>
