79c1bd42bdbc8e78f04aadc348130d0917f05129
russiaproject <ru5siaproject@yandex.com>
2025-06-23 21:02:59 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import json from '@rollup/plugin-json'; // Yeni ekle
 
 export default defineConfig({
   plugins: [
     react(),
-    json() // Bu satırı ekleyin
-  ],
-  build: {
-    rollupOptions: {
-      plugins: [json()] // Buraya da ekleyin
-    }
-  }
+    json() 
+  ]
 });
