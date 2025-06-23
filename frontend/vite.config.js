6a6487dd3042c24372680ab72e1e499722d1826f
russiaproject <ru5siaproject@yandex.com>
2025-06-23 21:23:22 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import json from '@rollup/plugin-json'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), json()],
+  plugins: [react()],
 })
