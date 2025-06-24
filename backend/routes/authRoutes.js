7f1cbec6aabc7fb8693f854f737c3b33a318465e
russiaproject <ru5siaproject@yandex.com>
2025-06-25 00:06:20 +0300

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { loginHandler } = require('../controllers/authController');
-
-const router = express.Router();
-
-router.post('/login', loginHandler);
-
-module.exports = router;
